9e6c20cd85e5cb214e443df813f545f38d2adbd5
Juan Varvello <jvarvello@gmail.com>
2023-05-12 12:29:54 -0300

    comentando funciones de taxi_assignment_solution

--- taxi_assignment_solution.cpp
+++ taxi_assignment_solution.cpp
@@ -2,33 +2,40 @@
 
 TaxiAssignmentSolution::TaxiAssignmentSolution() {}
 
+//Inicializa todas las asignacion en -1 para indicar que todavia no se hicieron
 TaxiAssignmentSolution::TaxiAssignmentSolution(int n) {
     this->_n = n;
     this->_taxi_assignment = std::vector<int>(n, -1);
     this->_pax_assignment = std::vector<int>(n, -1);
 }
 
+//Devuelve la cantidad de Taxis/Pasajeros
 int TaxiAssignmentSolution::getN() const {
     return this->_n;
 }
 
+//Asigna un Taxi a un Pasajero
 void TaxiAssignmentSolution::assign(int taxi, int pax) {
     this->_taxi_assignment[taxi] = pax;
     this->_pax_assignment[pax] = taxi;
 }
 
+//Checkea que un Taxi haya sido asignado
 bool TaxiAssignmentSolution::isTaxiAssigned(int taxi) const {
     return this->_taxi_assignment[taxi] != -1;
 }
 
+//Checkea que a un Pasajero ya se le haya asignado un Taxi
 bool TaxiAssignmentSolution::isPaxAssigned(int pax) const {
     return this->_pax_assignment[pax] != -1;
 }
 
+//Dado un Taxi, obtener el Pasajero al cual se lo asigno
 int TaxiAssignmentSolution::getAssignedPax(int taxi) const {
     return this->_taxi_assignment[taxi];
 }
 
+////Dado un Pasajero, obtener el Taxi que se le asigno
 int TaxiAssignmentSolution::getAssignedTaxi(int pax) const {
     return this->_pax_assignment[pax];
 }
